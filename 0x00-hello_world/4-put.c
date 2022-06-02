085de34687469eed4022178a0fcd47c24e91a8ff
Paul Sobande <paotec22@gmail.com>
2022-06-02 13:42:28 -0700

    Write a C program that prints given input without using printf

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,13 @@
+# include <studio.h>
+
+/**
+ * int main - Starting point
+ * void - no arguments been parsed, this will be in bracket
+ * start with a curly brace
+ */
+
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle");
+	return(0);
+}
