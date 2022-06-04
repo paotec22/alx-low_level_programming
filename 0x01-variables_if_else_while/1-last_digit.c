c33a3b8b8f1d3f7ccdac2dff693d700b6885c6ec
Paul Sobande <paotec22@gmail.com>
2022-06-04 00:34:18 -0700

    assign a random number to the variable n and print string based on parameters

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %i is %i and is ", n, (n % 10));
+
+	if ((n % 20) > 5)
+	{
+		printf("is greater than 5\n");
+	}
+	elseif ((n % 20) == 0)
+	{
+		printf("is 0\n");
+	}
+	else
+	{
+		printf("is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
