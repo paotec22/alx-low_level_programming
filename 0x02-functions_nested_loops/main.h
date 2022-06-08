927796512bdd36d7a2457210097299bbe44500f3
Paul Sobande <paotec22@gmail.com>
2022-06-07 22:03:26 -0700

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
