afedcea5014a603c0a7862ac9b8f82dac55c1a60
Paul Sobande <paotec22@gmail.com>
2022-07-05 16:37:26 -0700

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,48 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * new_dog - creates new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: a dog object
+ * new_dog - new cute dog.
+ *
+ *@name: name of dog.
+ *@age: age of dog.
+ *@owner: owner of dog.
+ *
+ * Return: Always 0.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, len1, len2;
-	dog_t *ndog;
-	char *a, *b;
+	dog_t *x;
 
-	len1 = 0;
-	for (i = 0; name[i] != '\0'; i++)
-		len1++;
-	len2 = 0;
-	for (i = 0; owner[i] != '\0'; i++)
-		len2++;
-	ndog = malloc(sizeof(dog_t));
-	if (ndog == NULL)
+	x = malloc(sizeof(dog_t));
+
+	if (x == NULL)
 		return (NULL);
-	a = malloc((len1 + 1) * sizeof(char));
-	if (a == NULL)
-	{
-		free(ndog);
-		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-		a[i] = name[i];
-	a[i] = '\0';
-	ndog->name = a;
-	ndog->age = age;
-	b = malloc((len2 + 1) * sizeof(char));
-	if (b == NULL)
-	{
-		free(a);
-		free(ndog);
-		return (NULL);
-	}
-	for (i = 0; i < len2; i++)
-		b[i] = owner[i];
-	b[i] = '\0';
-	ndog->owner = b;
-	return (ndog);
+
+	x->name = name;
+	x->age = age;
+	x->owner = owner;
+
+	return (x);
 }
