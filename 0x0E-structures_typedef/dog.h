084196631db68b775666ffff3136035fe03fdca1
Paul Sobande <paotec22@gmail.com>
2022-07-05 07:42:13 -0700

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,9 @@ struct dog
  * dog_t - new type for struct dog.
  */
 
+typedef struct dog_t;
+
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
