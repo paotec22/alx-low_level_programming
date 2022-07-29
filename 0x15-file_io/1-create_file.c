0e7b792e142cae8f921af57569b3e4c1dc47a09f
Paul Sobande <paotec22@gmail.com>
2022-07-29 10:47:36 -0700

    Function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <main.h>
+
+/**
+ * create_file: Create file with write only permissions if the file does mot exist.
+ * Truncate file if it exists; fill file with the contents of text_content.
+ * @filename: The name of the file to create
+ * @text_content: Content of the text
+ * Return: 1 if success, -1 if failed.
+ */
+
+inn create_file(const char *filename, char *text_content)
+{
+	ssize_t file;
+	ssize_t b_write;
+	size_t length;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	b_write = 0;
+	if (text_content)
+	{
+		for (length = 0; text_content[length]; lenth++);
+
+		b_write = write(file, text_content, length);
+	}
+	close(file);
+	if (b_write == -1)
+		return (-1);
+
+	return (1);
+}
