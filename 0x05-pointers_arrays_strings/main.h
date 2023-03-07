6ae9c8da4656445b1ce771dbaa0de2b84cd0cb42
Paul Sobande <36281797+paotec22@users.noreply.github.com>
2023-03-07 07:35:54 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,12 +5,13 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+//size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
